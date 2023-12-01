99efe51f5e12a0953a78d9ec4bf0b41adffa024a
anaisberg <anais.schlienger@lilo.org>
2023-12-01 11:01:39 +0100

    ✨ day 1 part 1

--- day-01/script.ts
+++ day-01/script.ts
@@ -1,10 +1,30 @@
 import { getInputContent } from "../helpers";
 
 const currentDay = "01";
-const allContents = getInputContent(currentDay);
+const isTest = false;
+const allContents = getInputContent(currentDay, isTest);
 
-const sums: number[] = [];
-let currentSum = 0;
+const calibrations: number[] = [];
+let calibrationSum = 0;
+
+// Part 1
+allContents.split(/\r?\n/).forEach((line: string) => {
+  let calibration: string[] = [];
+  line.split("").forEach((char: string) => {
+    if (!Number.isNaN(Number(char))) {
+      calibration.push(char);
+    }
+  });
+
+  const firstNumber = calibration[0];
+  const secondNumber = calibration.pop();
+  const newCalibration = Number(`${firstNumber}${secondNumber}`);
+  calibrations.push(newCalibration);
+  calibrationSum += newCalibration;
+});
+
+console.log("PART 1:");
+console.log(calibrationSum);
 
 allContents.split(/\r?\n/).forEach((line: string) => {
   if (line.length === 0) {
