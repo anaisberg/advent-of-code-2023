43e65b466ba2ce5c00b44d43300922779be07266
anaisberg <anais.schlienger@lilo.org>
2023-12-05 09:38:55 +0100

    ✨ day 04 - part 2

--- day-04/script.ts
+++ day-04/script.ts
@@ -25,3 +25,43 @@ allContents.split(/\r?\n/).forEach((line: string, index: number) => {
 
 console.log("PART 1:");
 console.log(points);
+
+// Part 2
+let cardsInDeck = 0;
+const cards: {
+  [key: number]: {
+    total: number;
+    cardsWon: number;
+  };
+} = {};
+
+allContents.split(/\r?\n/).forEach((line: string, index: number) => {
+  const card = line.split(" | ")[0].split(": ")[1].split(" ").map(Number);
+  const hand = line.split(" | ")[1].split(" ").map(Number);
+  let total = 0;
+  card.forEach((cardNumber) => {
+    const isNumberInHand = hand.includes(cardNumber);
+    if (isNumberInHand && cardNumber !== 0) {
+      total += 1;
+    }
+    return;
+  });
+  cards[index + 1] = { total, cardsWon: 1 };
+});
+
+Object.entries(cards)
+  .reverse()
+  .forEach(([cardNumber, card]) => {
+    const cardNumberInt = Number(cardNumber);
+    const total = card.total;
+    for (let i = cardNumberInt + 1; i <= cardNumberInt + total; i += 1) {
+      if (cards[i]) {
+        const { cardsWon: cardsWon_i } = cards[i];
+        cards[cardNumberInt].cardsWon += cardsWon_i;
+      }
+    }
+    cardsInDeck += cards[cardNumberInt].cardsWon;
+  });
+
+console.log("PART 2:");
+console.log(cardsInDeck);
